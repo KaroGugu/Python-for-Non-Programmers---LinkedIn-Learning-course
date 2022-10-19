eb41be420d0988346e0bae48e1ba10d61e1e5c1a
Karolina Gugudis <102053483+KaroGugu@users.noreply.github.com>
2022-10-19 09:18:29 +0100

    Add files via upload

--- number_guesser.py
+++ number_guesser.py
@@ -1,12 +1,12 @@
 import random
 import time
 
-print("Hi! Welcome to the guessing game. I'm picking a number between 1 and 100.")
-time.sleep(1)
-print("Pickinga number...")
-time.sleep(1)
+print("Hi! Welcome to the guessing game. I'll pick a number between 1 and 100. Can you guess it?")
+time.sleep(2)
+print("Wait while I pick a number...")
+time.sleep(1.5)
 
-client_number = int(input("What is your number guess?: "))
+client_number = int(input("I'm ready. So what is your number guess?: "))
 correct_number = random.randint(1, 100)
 guess_counter = 0
 
